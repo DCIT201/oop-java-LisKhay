a63c522c5fef8978a6a12a5ca707f51e6be14c2b
LisKhay <kobbykhay8899@gmail.com>
2024-12-21 16:12:22 +0000

    Add compiled .class files

--- src/exceptions/LoyaltyException.java
+++ src/exceptions/LoyaltyException.java
@@ -1,6 +1,8 @@
 package exceptions;
 
+// Custom exception class that extends the Exception class
 public class LoyaltyException extends Exception {
+    // Constructor accepting a message
     public LoyaltyException(String message) {
         super(message);
     }
--- src/exceptions/RentalException.java
+++ src/exceptions/RentalException.java
@@ -1,6 +1,8 @@
 package exceptions;
 
+// Custom exception class for rental-specific errors
 public class RentalException extends Exception {
+    // Constructor accepting a message
     public RentalException(String message) {
         super(message);
     }
